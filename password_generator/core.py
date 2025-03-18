2c16fcc20fa1a7031fa14f9a24b06b4b0e237b69
shauryasr04 <shauryasr059@gmail.com>
2025-03-17 21:24:44 -0400

    Updating core.py

--- password_generator/core.py
+++ password_generator/core.py
@@ -1,12 +1,30 @@
 import random
 import string
 import math
+import hashlib
 
-def generate(length):
-    password = ''.join(random.choices(string.ascii_letters, k=length))
+def generate(length, capitalize=False, numbers=False, symbols=False):
+    password = ''
+    list_lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    list_uppercase = [chr(ord(char) - 32) for char in list_lowercase]
+    list_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    list_symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ':', ';', ',', '.', '/', '?', '~']
+    rand_lower = [random.randint(0, len(list_lowercase) - 1) for _ in range(5)]
+    if capitalize:
+        rand_upper = [random.randint(0, len(list_uppercase) - 1) for _ in range(2)]
+    if numbers:
+        rand_number = [random.randint(0, len(list_numbers) - 1) for _ in range(3)]
+    if symbols:
+        rand_symbol = [random.randint(0, len(list_symbols) - 1) for _ in range(2)]
+    for i in range(length):
+        password += list_lowercase[rand_lower[i]]
+        if capitalize:
+            password += list_uppercase[rand_upper[i]]
+        if numbers:
+            password += list_numbers[rand_number[i]]
 
 #does a cesar cipher on a string
-def caesar_cipher(self, shift: int):
+def caesar_cipher(password, shift):
     if not (1 <= shift <= 25):
         raise ValueError("Shift value must be between 1 and 25.")
 
@@ -27,6 +45,15 @@ def scramble(self, string):
         letter = toList.pop(rand)
         retString = retString + letter
     return retString
+
+def binary_password(len):
+    retString = ''
+    for _ in range(len):
+        retString+= str(random.randint(0,1))
+    return retString
+
+def generateHash(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
 def getFunnyPassword():
     funny_list_1 = list('123456789')
